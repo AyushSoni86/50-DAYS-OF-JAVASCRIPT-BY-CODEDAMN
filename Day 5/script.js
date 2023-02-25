9e190f7274b06d33f39ab747bd21d5dd042be7c4
Ayush Lalchand Soni <ayusoni86@gmail.com>
2023-02-25 11:43:55 +0530

    Changes made

--- Day 5/script.js
+++ Day 5/script.js
@@ -1,2 +1,11 @@
 // Write a function which accepts a string argument and returns the count of characters between the first and last character 'X'
 
+const str = 'XeroX';
+
+function getTheGapX(str) {
+    // write your solution here
+
+    return
+}
+
+console.log(`Gap between the X's: ${getTheGapX(str)}`)
